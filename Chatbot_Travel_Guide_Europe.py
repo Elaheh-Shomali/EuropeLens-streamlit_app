7a6d005a055937b868d453779ea29d679ca8861f
Elaheh-Shomali <e.shomali.ph@gmail.com>
2025-01-27 08:19:44 +0100

    Update Chatbot_Travel_Guide_Europe.py

--- Chatbot_Travel_Guide_Europe.py
+++ Chatbot_Travel_Guide_Europe.py
@@ -1,19 +1,18 @@
-import os
-# Access the secret as an environment variable
-HF_TOKEN = os.getenv("HF_TOKEN")
-
 from langchain_huggingface import HuggingFaceEndpoint, HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
-
 import streamlit as st
+
+import os
+# Access the secret as an environment variable
+HF_TOKEN = os.getenv("HF_TOKEN")
 
 # llm
 hf_model = "mistralai/Mistral-7B-Instruct-v0.3"
-llm = HuggingFaceEndpoint(repo_id=hf_model, huggingfacehub_api_token=HF_TOKEN)
+llm = HuggingFaceEndpoint(repo_id=hf_model, huggingfacehub_api_token = HF_TOKEN)
 
 # embeddings
 embedding_model = "sentence-transformers/all-MiniLM-l6-v2"
@@ -24,10 +23,10 @@ embeddings = HuggingFaceEmbeddings(model_name=embedding_model,
 
 # load Vector Database
 # allow_dangerous_deserialization is needed. Pickle files can be modified to deliver a malicious payload that results in execution of arbitrary code on your machine
-vector_db_combined = FAISS.load_local("contents/faiss_index_europeLens", embeddings, allow_dangerous_deserialization=True)
+vector_db = FAISS.load_local("contents/faiss_index_europeLens", embeddings, allow_dangerous_deserialization=True)
 
 # retriever
-retriever = vector_db_combined.as_retriever(search_kwargs={"k": 2})
+retriever = vector_db.as_retriever(search_kwargs={"k": 2})
 
 # prompt
 template = """You are a nice chatbot having a conversation with a human. Answer the question based only on the following context and previous conversation. Keep your answers short and succinct.
@@ -62,12 +61,12 @@ rag_bot = init_bot()
 st.title("EuropeLens")
 
 # Markdown
-st.markdown("""
-Welcome to EuropeLens! 🌟
-
-Ask me anything about Europe, and I’ll bring you the answers—culture, history, travel tips, and more!
-
-""")
+#st.markdown("""
+#Welcome to EuropeLens! 🌟
+#
+#Ask me anything about Europe, and I’ll bring you the answers—culture, history, travel tips, and more!
+#
+#""")
 
 # Initialise chat history
 # Chat history saves the previous messages to be displayed
@@ -103,5 +102,3 @@ if prompt := st.chat_input("Curious minds wanted!"):
 
         # Add assistant response to chat history
         st.session_state.messages.append({"role": "assistant", "contents":  response})
-
-
